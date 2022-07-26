1d3f88e032f12f66dd4288db72a7ac5d10b33526
daichi-opsin <daichi.sato@ops-in.com>
2022-07-26 16:12:13 +0900

    コミット

---
+++ spec/system/tests_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Tests", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  context "テスト" do
+    it "NG" do
+      expect(false).to eq(true)
+    end
+  end
+end
